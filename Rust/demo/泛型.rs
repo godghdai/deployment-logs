1cee234265406c7c1283f5f7a003bf2e745e6fd9
godghdai <godghdai@gmail.com>
2021-09-11 03:16:18 +0800

    Update 泛型.rs

--- Rust/demo/泛型.rs
+++ Rust/demo/泛型.rs
@@ -4,9 +4,16 @@ fn add<T:Display+ std::ops::Add<Output = T>>(x:T,y:T)->T{
     x+y
 }
 
+fn opt<T>(fun:fn(T,T) -> T,x:T,y:T)->T{
+    fun(x,y)
+}
+
 fn main() {
     let res=add(1,2);
     println!("{}",res);
     let res=add(1.2,2.3);
     println!("{}",res);
+    
+    println!("{}",opt::<i32>(|x,y|x+y,1,2));
+    println!("{}",opt::<i32>(|x,y|x*y,3,2));
 }
